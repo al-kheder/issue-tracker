b129260bcca23ff19fe03d34416666ccf3567e4c
ali al-kheder <alkhdyyr@gmail.com>
2025-09-09 22:45:44 +0200

    create new isseue form

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
       >
         <Theme>
           <NavBar />
-          <main>{children}</main>
+          <main className="p-8">{children}</main>
         </Theme>
       </body>
     </html>
