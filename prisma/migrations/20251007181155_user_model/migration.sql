c0b8984be201f1d2e4d63be5a14bd57001671d82
ali al-kheder <alkhdyyr@gmail.com>
2025-10-07 20:23:56 +0200

    add prisma addapter

---
+++ prisma/migrations/20251007181155_user_model/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `VerificationToken` (
+    `identifier` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`identifier`, `token`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
