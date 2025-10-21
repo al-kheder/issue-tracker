60dcfadafc769a4a4fe0c9f13a18beeb0b9fd82d
ali al-kheder <alkhdyyr@gmail.com>
2025-10-21 11:03:52 +0200

    fix: remove unnecessary blank line in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,6 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-
 export const metadata: Metadata = {
   title: "issues tracker App",
   description: "issues tracker to keep your Productivity ",
